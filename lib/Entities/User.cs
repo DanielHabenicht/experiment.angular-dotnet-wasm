2be924cfcc68826073241070b33d4d05d8bbe87e
DanielHabenicht <daniel-habenicht@outlook.de>
2022-06-19 22:05:38 +0000

    add lib project

---
+++ lib/Entities/User.cs
@@ -0,0 +1,7 @@
+namespace HelloWorld;
+
+public class User
+{
+    public string Name { get; set; }
+    public string Age { get; set; }
+}
---
+++ lib/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using DotNetJS;
+using Microsoft.JSInterop;
+
+namespace HelloWorld;
+
+public partial class Program
+{
+    // Entry point is invoked by the JavaScript runtime on boot.
+    public static void Main()
+    {
+        // Invoking 'dotnet.HelloWorld.GetHostName()' JavaScript function.
+        var hostName = GetHostName();
+        // Writing to JavaScript host console.
+        Console.WriteLine($"Hello {hostName}, DotNet here!");
+    }
+
+    [JSFunction] // The interoperability code is auto-generated.
+    public static partial string GetHostName();
+
+    [JSInvokable] // The method is invoked from JavaScript.
+    public static string GetName() => "DotNet";
+
+
+    [JSInvokable] // The method is invoked from JavaScript.
+    public static User GetUser() => new User { Name = "DotNet", Age = "JS" };
+}
